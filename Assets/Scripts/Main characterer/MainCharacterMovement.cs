ff86204f9c22ee752e766c35a271b93e707e0be5
Danilo Costa <p.danilo.98@hotmail.com>
2022-09-08 11:49:19 -0300

    Mudanças bugadas pra krl

--- Assets/Scripts/Main characterer/MainCharacterMovement.cs
+++ Assets/Scripts/Main characterer/MainCharacterMovement.cs
@@ -54,16 +54,16 @@ public class MainCharacterMovement : MonoBehaviour
             GetComponent<EchoEffect>().enabled = true;
             if (IsMovingRight())
             {
-                transform.GetChild(0).gameObject.active = true;
+                transform.GetChild(0).gameObject.SetActive(true);
             }
             else
             {
-                transform.GetChild(0).gameObject.active = false;
+                transform.GetChild(0).gameObject.SetActive(false);
             }
         }
         else
         {
-            transform.GetChild(0).gameObject.active = false;
+            transform.GetChild(0).gameObject.SetActive(false);
             GetComponent<EchoEffect>().enabled = false;
         }
             
@@ -177,6 +177,7 @@ public class MainCharacterMovement : MonoBehaviour
             return false;
         }
     }
+
     private void countPoints(){
         float x;
         if(player.position.x > previusX){
